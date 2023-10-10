47ee7969288715fe0962f382e48e24175f6d8a8e
Nate Reprogle <4064388+TerrrorByte@users.noreply.github.com>
2023-10-10 17:48:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Plugin Messenger - Paper
+
+This plugin was written for my [Velocity plugin development tutorial series](https://www.youtube.com/playlist?list=PL51GdsaNM19JlY0t6KNyi4vtkC5Q-4-F4) and designed to be paired with the Plugin Messenger - Velocity plugin.
+Plugin messaging has been around since Minecraft 1.1, which was released in 2012, over 11 years ago!
+
+It's very simple, it implements two commands:
+1. /broadcast - A broadcast command that works with MiniMessage and sends it to all players on all servers
+2. /creeper - A command that requests creepers be spawned at all players on all servers
+
+It demos the following concepts:
+1. Working with Byte Streams and data outputs/inputs
+2. Plugin Messaging using the default BungeeCord plugin messaging commands
+3. Plugin Messaging using our own custom channels
+
+For more information related to Plugin Messaging, please see the following links
+* https://wiki.vg/Plugin_channels
+* https://web.archive.org/web/20220711204310/https://dinnerbone.com/blog/2012/01/13/minecraft-plugin-channels-messaging/
+
+I hope you learned something!
+- Nate (TerrorByte)
---
+++ src/main/java/org/reprogle/pluginmessenger/PluginMessenger.java
@@ -0,0 +1,81 @@
+package org.reprogle.pluginmessenger;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
+
+public final class PluginMessenger extends JavaPlugin implements CommandExecutor, PluginMessageListener {
+
+    public static PluginMessenger plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        this.getCommand("broadcast").setExecutor(this);
+        this.getCommand("creeper").setExecutor(this);
+        this.getLogger().info("Plugin messenger booted up!");
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "demo:main");
+        getServer().getMessenger().registerIncomingPluginChannel(this, "demo:main", this);
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Plugin messenger shutting down");
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(command.getName().equals("broadcast")) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("MessageRaw");
+            out.writeUTF("ALL");
+
+
+            StringBuilder builder = new StringBuilder();
+            for (String string : args) {
+                builder.append(string).append(" ");
+            }
+
+            String message = builder.toString().trim();
+            out.writeUTF(GsonComponentSerializer.gson().serialize(MiniMessage.miniMessage().deserialize(message)));
+
+            this.getServer().sendPluginMessage(this, "BungeeCord", out.toByteArray());
+            sender.sendMessage("Broadcasting your message!");
+
+        } else if (command.getName().equals("creeper")){
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("RequestCreeper");
+
+            this.getServer().sendPluginMessage(this, "demo:main", out.toByteArray());
+            sender.sendMessage("Requesting creepers be spawned near all players on all servers");
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] bytes) {
+        if(!channel.equals("demo:main")) return;
+
+        ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
+        String subchannel = in.readUTF();
+
+        if(subchannel.equals("SpawnCreeper")) {
+            Location location = player.getLocation();
+            location.getWorld().spawnEntity(location, EntityType.CREEPER);
+        }
+    }
+}
